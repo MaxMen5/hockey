a40a2fee7b692793729b918f472ff8ab87a404f2
MaxMen5 <161643770+MaxMen5@users.noreply.github.com>
2024-12-13 01:21:26 +0300

    Authorization.

--- src/main/java/ru/mendeleev/hockey/gui/LogInDialog.java
+++ src/main/java/ru/mendeleev/hockey/gui/LogInDialog.java
@@ -1,7 +1,56 @@
 package ru.mendeleev.hockey.gui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
 
 public class LogInDialog extends JDialog {
-    // TODO
+
+    private static final String TITLE = "Авторизация";
+
+    private final JTextField loginField = new JTextField();
+    private final JTextField passwordField = new JTextField();
+
+    public LogInDialog() {
+        setTitle(TITLE);
+
+        JPanel mainPanel = new JPanel(new GridLayout(3, 1));
+
+        JPanel loginPanel = new JPanel(new BorderLayout());
+        JPanel passwordPanel = new JPanel(new BorderLayout());
+
+        loginPanel.add(new JLabel("Логин: "), BorderLayout.WEST);
+        loginPanel.add(loginField, BorderLayout.CENTER);
+
+        passwordPanel.add(new JLabel("Пароль: "), BorderLayout.WEST);
+        passwordPanel.add(passwordField, BorderLayout.CENTER);
+
+        mainPanel.add(loginPanel);
+        mainPanel.add(passwordPanel);
+        mainPanel.add(new JButton(new ButtonAction()));
+
+        setModal(true);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    }
+
+    private class ButtonAction extends AbstractAction {
+        ButtonAction() {
+            putValue(NAME,  "Авторизоваться");
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            if (loginField.getText().equals("login") || passwordField.getText().equals("password")) {
+                // TODO
+                dispose();
+            }
+            else {
+                loginField.setText("");
+                passwordField.setText("");
+            }
+        }
+    }
+
 }
